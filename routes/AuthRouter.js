38730c053ea8262403b7ae1a6d0b9432d811c8ba
Yusuf Eryilmaz <yusuferyilmaz1819@gmail.com>
2021-11-09 22:51:47 -0500

    package.json is updated

--- routes/AuthRouter.js
+++ routes/AuthRouter.js
@@ -1,5 +1,4 @@
 const express = require("express");
-const { check } = require("express-validator");
 const AuthController = require("../controllers/AuthController");
 const validations = require("../middleware/validationMiddleware");
 
